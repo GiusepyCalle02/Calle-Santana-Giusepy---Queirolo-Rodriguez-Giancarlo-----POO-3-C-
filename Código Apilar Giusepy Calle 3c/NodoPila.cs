68d183b6a1b39cad1a47ea34fd938fcce852220d
GiusepyCalle02 <86169564+GiusepyCalle02@users.noreply.github.com>
2021-06-20 15:53:11 -0500

    Add files via upload
    Tarea de TAD Pila, Cola, y Herencia

---
+++ Código Apilar Giusepy Calle 3c/NodoPila.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Código_Apilar_Giusepy_Calle_3c
+{
+    class NodoPila
+    {
+        public static Stack<int> CrearPila()
+        {
+            Stack<int> pila = new Stack<int>();
+            Console.WriteLine("Programador... has creado una pila");
+            return pila;
+        }
+
+        public static void imprimirPila(Stack<int> pila)
+        {
+            if (pila == null)
+                Console.WriteLine("Hey... inicializa la pila primero");
+            else if (pila.Count == 0)
+                Console.WriteLine("Primero debes agregar valores a la pila");
+            else
+            {
+                int[] Imprimir = pila.ToArray();
+                int contador = pila.Count;
+                Console.WriteLine("Elementos: ");
+                for (int i = 0; i < contador; i++)
+                {
+                    Console.WriteLine(Imprimir[i]);
+                }
+
+            }    
+            Console.WriteLine();
+        }
+
+        public static Stack<int> pushPila(Stack<int> pila)
+        {
+
+            if (pila == null)
+            {
+                Console.WriteLine("Hey... inicializa la pila primero");
+            }
+            else
+            {
+                Console.WriteLine("Ingresa Valor a apilar (PUSH)");
+                int valor = Int16.Parse(Console.ReadLine());
+                pila.Push(valor);
+                Console.WriteLine("Ingreso exitoso..");
+                Console.WriteLine();
+            }
+                return pila;
+            
+
+        }
+
+        public static Stack<int> popPila(Stack<int> pila)
+        {
+            if (pila.Count == 0)
+                Console.WriteLine("Hey... inicializa la pila primero");
+            else
+            {
+                Console.WriteLine("POP a la pila: " + pila.Pop());
+                Console.WriteLine("Sacaste un elemento.");
+            }
+               
+            return pila;
+
+        }
+
+        public static void Peek(Stack<int> pila)
+        {
+            if (pila == null)
+                Console.WriteLine("Hey... primero inicializa la Pila");
+            else if (pila.Count == 0)
+                Console.WriteLine("Primero debes agregar valores a la Pila");
+            else
+            {
+                Console.WriteLine("Elemento en la cima de la Pila: " + pila.Peek());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Código Apilar Giusepy Calle 3c/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Código_Apilar_Giusepy_Calle_3c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> miPila = null;
+            
+            while (true)
+            {
+                Console.WriteLine("Bienvenido a la Estructura de Datos LIFO");
+                Console.WriteLine("Opción 1. Crear la pila");
+                Console.WriteLine("Opción 2. Ingresar elemento(PUSH)");
+                Console.WriteLine("Opción 3. Sacar elemento (POP)");
+                Console.WriteLine("Opción 4. Método miron (PEEK)");
+                Console.WriteLine("Opción 5. Imprimir toda la pila");
+                Console.WriteLine("Opción 6. Salir");
+                int teclado = Int16.Parse(Console.ReadLine());
+                switch (teclado)
+                {
+                    case 1: //Crear
+                        miPila = NodoPila.CrearPila();
+                        break;
+                    case 2: //PUSH 
+                        miPila = NodoPila.pushPila(miPila);
+                        break;
+                    case 3: //POP
+                        miPila = NodoPila.popPila(miPila);
+                        break;
+                    case 4: //PEEK
+                        NodoPila.Peek(miPila);
+                        break;
+                    case 5: //Imprimir
+                        Console.WriteLine("Imprimiendo la pila...");
+                        NodoPila.imprimirPila(miPila);
+                        break;
+
+                    case 6: break;
+                }
+
+                if (teclado == 6)
+                {
+                    Console.WriteLine("Gracias por usar el programa");
+                    break;
+                }
+
+            }
+        }
+    }
+}
